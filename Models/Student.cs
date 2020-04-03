2ee37b8732d902f6920669a879dfe054d199a0fd
ketansharma97 <97.ketansharma@gmail.com>
2020-04-03 10:42:58 +0530

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentGitIgnore.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public string Marks { get; set; }
+    }
+}
